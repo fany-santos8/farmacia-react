d7d7fa1fb7d0c2cbf9890285076e2b4808512ff3
fany-santos8 <estefany_fanysantos@hotmail.com>
2025-01-27 14:58:00 -0300

    Enviando logica do deletar tema

--- src/components/temas/cardtemas/CardTemas.tsx
+++ src/components/temas/cardtemas/CardTemas.tsx
@@ -15,15 +15,16 @@ function CardTemas({ tema }: CardTemasProps) {
             
             <div className="flex">
             <Link to={`/editartema/${tema.id}`}
-	className='w-full text-slate-100 bg-	indigo-400 hover:bg-indigo-800 
-    flex items-center justify-center py-2'>
-	<button>Editar</button>
-</Link>
+	            className='w-full text-slate-100 bg-	indigo-400 hover:bg-indigo-800 
+                flex items-center justify-center py-2'>
+	           <button>Editar</button>
+            </Link>
 
-                <Link to='' className='text-slate-100 bg-red-400 hover:bg-red-700 w-full 
-                    flex items-center justify-center'>
-                    <button>Deletar</button>
-                </Link>
+           <Link to={`/deletartema/${tema.id}`} 
+	            className='text-slate-100 bg-red-400 hover:bg-red-700 w-full 
+		        flex items-center justify-center'>
+	            <button>Deletar</button>
+            </Link>
             </div>
 
         </div>
